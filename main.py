660847b6299b66bada957a9b54039b90e85068bb
brakahaugen <espenbragerhaug@gmail.com>
2021-03-04 18:05:31 +0100

    inital commit

---
+++ main.py
@@ -0,0 +1,171 @@
+from util import *
+import pandas as pd
+from glob import glob
+from random import random, shuffle, uniform, randint
+import itertools
+
+
+def get_n_random_trees(num_trees, tree_glob = "test_trees/*.las"):
+    """
+    Returns n random trees from the given directory
+    the format of the returned trees are loaded pyntcloud objects
+    """
+    files = glob(tree_glob)
+    shuffle(files)
+    files = files[:num_trees] if len(files) >= num_trees else files
+
+    trees = []
+    for file in files:
+        trees.append(PyntCloud.from_file(file))
+
+    return trees
+
+
+def put_in_origo(pc: pd.DataFrame, cols: list):
+    """
+    Sets the minimum point of the dataframe into origo, and translates all other points accordingly
+    Returns: Dataframe with points in range [0 -> (xmax-xmin)]
+    """
+    pc[cols] = pc[cols].apply(lambda x: (x - x.min()), axis=0)
+    return pc
+
+def get_maximum_xyz(pcs: list, cols: list):
+    """
+    returns the maximum x, y and z value from the given pcs (pointclouds)
+    """
+    max_vals = [0,0,0]
+
+    for tree in pcs:
+        tree = tree.points
+        for i in range(len(cols)):
+            if tree[cols[i]].max() > max_vals[i]:
+                max_vals[i] = tree[cols[i]].max()
+
+    return max_vals
+
+def normalize_trees_on_max_xyz(random_trees: list, cols: list, col_max_values: list):
+    """
+    For each tree in the random trees normalize the cols on the col max values
+    Returns -> list of trees in range [0->1]
+    """
+    for tree in random_trees:
+        for col, col_max in zip(cols, col_max_values):
+            tree.points[col] = tree.points[col].div(col_max)
+    return random_trees
+
+def preprocess_trees(random_trees: list):
+    """
+    For each tree, put the lowest point in origo
+    For all trees, find maximum value, divide all trees on this maxima.
+    For all trees, scatter 
+    """
+    cols = ['x','y','z']
+
+    for tree in random_trees:
+        tree.points = put_in_origo(tree.points, cols)
+    col_max_values = get_maximum_xyz(random_trees, cols)
+    random_trees = normalize_trees_on_max_xyz(random_trees, cols, col_max_values)
+
+        
+    return random_trees
+
+def create_random_space(width, height, num_trees):
+    """
+    Returns a list with size num_trees of random, unique integer pairs
+    """
+    width = width - 1
+    height = height -1
+
+    assert(num_trees < width * height), "Not big enough grid to create that many separated trees"
+
+    random_pairs = []
+    x, y = randint(0, width), randint(0, height)    
+
+    while len(random_pairs) < num_trees:
+        random_pairs.append((x, y))
+        x, y = randint(0, width), randint(0, height)
+        while (x, y) in random_pairs:
+            x, y = randint(0, width), randint(0, height)
+
+    return random_pairs
+        
+        
+def scatter_trees_on_grid(random_trees: list, width_height: list, allow_clustering: bool, augmentation: dict):
+    """
+        Scatters the trees randomly on a grid
+        Returns a PyntCloud with all the trees scattered and merged into one
+    """
+    axes = ["x","y"]
+    labels = []
+    
+    if allow_clustering:
+        for tree in random_trees:
+            label = []
+            for axis, max in zip(axes, width_height):
+                tree.points[axis] = tree.points[axis].add(round(uniform(0,max-1), 3))
+                label.append(tree.points[axis].min())
+                label.append(tree.points[axis].max())
+            labels.append(label)
+    
+    if not allow_clustering:
+        for tree, offsets in zip(random_trees, create_random_space(*width_height, len(random_trees))):
+            label = []
+            for axis, val in zip(axes, offsets):
+                tree.points[axis] = tree.points[axis].add(val)
+                label.append(tree.points[axis].min())
+                label.append(tree.points[axis].max())
+            labels.append(label)
+    
+    print("labels: LABELS ARE ON FORMAT: x_min, x_max, y_min, y_max\n",labels) 
+    
+    # print("single tree", random_trees[0].points)
+    merged_trees = random_trees[0]
+    for tree in random_trees[1:]:
+        merged_trees.points = pd.concat([merged_trees.points, tree.points])
+
+    print(merged_trees.points)
+
+    return labels, merged_trees
+
+# my1 = PyntCloud.from_file("test_trees/1.las")
+# my2 = PyntCloud.from_file("test_trees/2.las")
+# ts = preprocess_trees([my1,my2])
+# # print(ts[0].points, ts[1].points)
+
+
+
+def get_random_sample(num_trees = 32, width: int = 8, height: int = 8, resolution: int = 256, allow_clustering: bool = True, augmentation: dict = {}):
+    print("getting")
+    random_trees = get_n_random_trees(num_trees)
+    print("processing")
+    random_trees = preprocess_trees(random_trees)
+    print("scattering")
+    labels, scattered_trees_on_grid = scatter_trees_on_grid(random_trees, [width, height], allow_clustering=allow_clustering, augmentation=augmentation)
+    
+    print("normalizing")
+    scattered_trees_on_grid.points = normalize(scattered_trees_on_grid.points)
+    
+    print("creating label image")
+    label_image = create_label_image(labels, width, height, resolution)
+
+    print("Creating sequences")
+    sequences = TDI(scattered_trees_on_grid.points, 3)
+    images = []
+    print("Mapping")
+    for seq in sequences:
+        images.append(Mapping_M(seq, r = resolution))
+    
+
+    print(images[0])
+    print("concattenating images")
+    concatted_image = np.append(np.append(images[0], images[1], axis=1), images[2], axis=1)
+    cv2.imshow("Window", concatted_image)
+    cv2.waitKey(0)
+    cv2.imshow("Windows", label_image)
+    cv2.waitKey(0)
+    return concatted_image #A concatted image with the three images
+
+
+if __name__ == "__main__":
+    print("starting program")
+    get_random_sample()
---
+++ util.py
@@ -0,0 +1,121 @@
+from pyntcloud import PyntCloud
+import pandas as pd
+import math
+import numpy as np
+from tqdm import tqdm
+import cv2
+
+
+
+def TDI(points: pd.DataFrame, k = 3):
+    """
+    Adds a column 'slice' to the dataframe. the k slices are equal in length
+    given k=3:
+        slice 1 is the scattered shrub
+        slice 2 is the rod-shaped trunk
+        slice 3 is the spherical canopy
+    returns a list of the sequences 
+    """
+
+    points['slice'] = points.apply(lambda row: math.ceil(row["z"] * k) if math.ceil(row["z"] * k) > 0 else 1, axis=1)
+
+    slices = []
+    for s in range(1,k +1):
+        slices.append(points[points['slice'] <= s])
+
+    return slices
+
+def create_label_image(labels: list, width: int, height: int, resolution: int):
+    """
+    Creating a image of the given labels, with given resolution and gridsize
+    """
+    if width != height:
+        assert(False), "Width is not equals height, we have not implemented this case yet"
+
+    labels = ((np.asarray(labels, dtype=np.float32)/width) * resolution) # normalize labels
+    image = np.zeros((resolution, resolution))
+    label_id = 1
+
+    for xmin, xmax, ymin, ymax in labels:
+        for i in range(int(np.floor(xmin)), int(np.ceil(xmax))):
+            for j in range(int(np.floor(ymin)), int(np.ceil(ymax))):
+                image[i,j] = label_id
+        label_id += 1
+
+    return image
+
+
+def Mapping_M(slice: pd.DataFrame, r: int = 256):
+    n = slice.shape[0]
+
+    x_list = list(slice['x'])
+    y_list = list(slice['y'])
+    # print(slice.iloc[0])
+    
+    I = np.zeros((r,r,3))
+    C = {} #C = (r, r)
+    for i in range(r):
+        for j in range(r):
+            C[i,j] = []
+
+    for j in tqdm(range(n)):
+        x = math.floor(x_list[j] * r) if math.floor(x_list[j] * r) < r else r-1
+        y = math.floor(y_list[j] * r) if math.floor(y_list[j] * r) < r else r-1
+        I[x,y,0] = I[x,y,0] + 1 #Appending to the density
+        C[x,y].append(slice.iloc[j]['x'])
+
+    for h in tqdm(range(r)):
+        for t in range(r):
+            z = C[h,t]
+            val = abs(max(z) - min(z)) if z else 0
+            I[h,t,1] = val
+
+    for h in tqdm(range(r)):
+        for t in range(r):
+            grad_h = 0
+            for i in [-1,0,1]:
+                for j in [-1,0,1]:
+                    if h+i >= r or h+i < 0 or t+j >= r or t+j < 0:
+                        continue
+                    grad_h += abs(I[h+i,t+j,1] - I[h,t,1])
+            I[h,t,2] = grad_h
+    # normalize each channel from 0 to 255
+    print(I.shape)
+    print(I.shape[2])
+
+
+    scale_factors = 255 / np.amax(I,axis=(0,1))
+    print("LETS GO")
+    print(scale_factors)
+    for z in range(I.shape[2]):
+        for x in range(I.shape[0]):
+            for y in range(I.shape[1]):
+                I[x,y,z] = scale_factors[z]*I[x,y,z]
+    print(I)
+    return I#np.floor((I/r) * 256).astype(np.uint8)
+
+
+def normalize(pc: pd.DataFrame):
+    cols = ['x','y','z']
+    pc[cols] = pc[cols].apply(lambda x: (x - x.min()), axis=0).apply(lambda x: (x/x.max()), axis=0)
+    return pc
+
+if __name__ == '__main__':    
+    my_point_cloud = PyntCloud.from_file("input/mini-pc-segment-no-ground.las")
+    """NORMALIZE POINTS"""
+    my_point_cloud.points = normalize(my_point_cloud.points)
+
+    sequences = TDI(my_point_cloud.points, 3)
+
+    r = 256
+
+    images = []
+
+    for seq in sequences:
+        images.append(Mapping_M(seq, r = r))
+
+    print(images[0].shape)
+    concatted_image = np.append(np.append(images[0], images[1], axis=1), images[2], axis=1)
+    print(concatted_image.shape)
+    cv2.imshow("Window", concatted_image)
+    cv2.waitKey(0)
